cc506c03cb6f8406a4fabb4294607ee2ff65c0bc
Volker Wiegand <volker@railduino.de>
2026-01-07 23:59:37 +0100

    Copy RSA-Key-Pair with gdt sync

--- sync/cmd.go
+++ sync/cmd.go
@@ -1,7 +1,9 @@
 package sync
 
 import (
+	"github.com/railduino/gd-tools/agent"
 	"github.com/railduino/gd-tools/config"
+	"github.com/railduino/gd-tools/utils"
 	"github.com/urfave/cli/v2"
 )
 
@@ -15,17 +17,50 @@ var Command = &cli.Command{
 	Description: Describe,
 	Flags: []cli.Flag{
 		config.FlagVerbose,
+		&cli.BoolFlag{
+			Name:  "ssh-key",
+			Usage: "also copy the ssh key pair",
+		},
 	},
 	Action: Run,
 }
 
 func Run(c *cli.Context) error {
-	cfg, err := config.ReadConfig(c)
+	cfg, req, err := config.ReadConfigPlus(c)
 	if err != nil {
 		return err
 	}
 
 	cfg.PushCerts()
+
+	if c.Bool("ssh-key") {
+		privContent, pubContent, err := utils.GetRSAKeyPair(cfg.FQDN())
+		if err != nil {
+			return err
+		}
+		privFile := agent.File{
+			Task:    "write",
+			Path:    agent.GetRootDir(".ssh", "id_rsa"),
+			Content: privContent,
+			Mode:    "0600",
+			User:    "root",
+			Group:   "root",
+		}
+		req.AddFile(&privFile)
+		pubFile := agent.File{
+			Task:    "write",
+			Path:    agent.GetRootDir(".ssh", "id_rsa.pub"),
+			Content: pubContent,
+			Mode:    "0600",
+			User:    "root",
+			Group:   "root",
+		}
+		req.AddFile(&pubFile)
+
+		if err := req.Send(); err != nil {
+			return err
+		}
+	}
 
 	return nil
 }
